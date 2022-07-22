110f2ebd3a9a24861403a2e615f2b2fa2782a1d2
alovekil <alovekil176@gmail.com>
2022-07-22 16:48:07 +0400

    Test

--- app/src/main/java/com/example/testalovekil2/MainActivity.kt
+++ app/src/main/java/com/example/testalovekil2/MainActivity.kt
@@ -1,15 +1,47 @@
 package com.example.testalovekil2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.example.testalovekil2.View.Fragment_Qeydiyyat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var button : Button
+    lateinit var button1 : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.btn_register)
+        button1 = findViewById(R.id.btn_login)
+        button.setOnClickListener(View.OnClickListener() {
+            fun onClick(view: View) {
+                val intent = Intent(this@MainActivity, Fragment_Qeydiyyat::class.java)
+                Toast.makeText(
+                    this@MainActivity,
+                    "Qeydiyyat səhifəsinə keçid edildi",
+                    Toast.LENGTH_SHORT
+                ).show()
+                startActivity(intent)
+            }
+
+        })
+
+        button1.setOnClickListener(View.OnClickListener {
+            fun onClick(view: View){
+                Toast.makeText(
+                    this@MainActivity,
+                    "Giris səhifəsinə keçid edildi",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+
+
 
     }
-}
+    }
--- app/src/main/java/com/example/testalovekil2/View/Fragment_Qeydiyyat.kt
+++ app/src/main/java/com/example/testalovekil2/View/Fragment_Qeydiyyat.kt
@@ -1,11 +1,13 @@
 package com.example.testalovekil2.View
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.testalovekil2.R
+import kotlinx.android.synthetic.main.fragment__qeydiyyat.*
 
 
 class Fragment_Qeydiyyat : Fragment() {
@@ -21,6 +23,16 @@ class Fragment_Qeydiyyat : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment__qeydiyyat, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lawyerregistr.setOnClickListener(View.OnClickListener {
+            fun onClick(view: View) {
+                val intent = Intent(requireContext(),Fragment_Vekil_Qeydiyyat::class.java)
+                startActivity(intent)
+            }
+            })
     }
 
 }

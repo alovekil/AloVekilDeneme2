02b45b57b3ba9dcf892235e8fa0361996bd9a74a
alovekil <alovekil176@gmail.com>
2022-07-23 23:16:53 +0400

    Update Fragment_Qeydiyyat.kt

--- app/src/main/java/com/example/testalovekil2/View/Fragment_Qeydiyyat.kt
+++ app/src/main/java/com/example/testalovekil2/View/Fragment_Qeydiyyat.kt
@@ -30,6 +30,7 @@ class Fragment_Qeydiyyat : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        
     }
 
 }

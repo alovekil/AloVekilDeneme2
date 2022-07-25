a5883df603d1d0affc765983c853989c4cf7f282
alovekil <alovekil176@gmail.com>
2022-07-25 20:33:05 +0400

    Test

--- app/src/main/java/com/example/testalovekil2/View/OTPFragment.kt
+++ app/src/main/java/com/example/testalovekil2/View/OTPFragment.kt
@@ -20,6 +20,7 @@ import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
 import kotlinx.android.synthetic.main.fragment_o_t_p.*
+import kotlinx.android.synthetic.main.fragment_o_t_p.view.*
 import java.util.concurrent.TimeUnit
 
 
@@ -38,79 +39,6 @@ class OTPFragment : Fragment() {
     private  fun Tag() = "MAIN_TAG"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        icontelephone.visibility=View.GONE
-        otplayout.visibility=View.GONE
-
-
-        firebaseAuth= FirebaseAuth.getInstance()
-
-        progressDialog= ProgressDialog(activity)
-        progressDialog.setTitle("text")
-        progressDialog.setCanceledOnTouchOutside(false)
-
-        mCallBack=object :PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
-            override fun onVerificationCompleted(p0: PhoneAuthCredential) {
-                Log.d(Tag(),"onverficationcompleted: ")
-                signinwithphoneauth(p0)
-            }
-
-            override fun onVerificationFailed(p0: FirebaseException) {
-            progressDialog.dismiss()
-                Log.d(Tag(),"onVerficationfailed ${p0}")
-                Toast.makeText(activity,"${p0.message}",Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onCodeSent(p0: String, p1: PhoneAuthProvider.ForceResendingToken) {
-               Log.d(Tag(),"onCodesent:${p0}")
-                mVerificationId=p0
-                forceResendingToken=p1
-                progressDialog.dismiss()
-                phoneEt.visibility=View.GONE
-                verificationcode.visibility=View.VISIBLE
-                Toast.makeText(activity,"Verification Code sent...",Toast.LENGTH_SHORT).show()
-                captioncode.text="Please type the verification code we sent to ${phoneEt.text.toString().trim()}"
-                super.onCodeSent(p0, p1)
-            }
-
-
-        }
-
-        continuebtn.setOnClickListener{
-            //input phone number
-        val phone=phoneEt.text.toString().trim()
-            //validate phone number
-            if(TextUtils.isEmpty(phone)){
-                Toast.makeText(this.activity,"Please enter your phone number...",Toast.LENGTH_SHORT).show()
-
-            }
-            else{
-                startphoneverification(phone)
-            }
-        }
-
-        otpresentcode.setOnClickListener {
-            val phone=phoneEt.text.toString().trim()
-            //validate phone number
-            if(TextUtils.isEmpty(phone)){
-                Toast.makeText(this.activity,"Please enter your phone number...",Toast.LENGTH_SHORT).show()
-
-            }
-            else{
-                resendphoneOTPcode(phone,forceResendingToken)
-            }
-        }
-
-        submit_btn.setOnClickListener {
-            val phone=phoneEt.text.toString().trim()
-            //validate phone number
-            if(TextUtils.isEmpty(phone)){
-                Toast.makeText(this.activity,"Please enter your phone number...",Toast.LENGTH_SHORT).show()
-
-            }
-            else{
-                resendphoneOTPcode(phone,forceResendingToken)
-            }
-            }
 
     }
     private fun startphoneverification(phone: String){
@@ -165,8 +93,82 @@ class OTPFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_o_t_p, container, false)
+        val view=inflater.inflate(R.layout.fragment_o_t_p, container, false)
+
+        view.icontelephone.visibility=View.GONE
+        view.telnumberlayout.visibility=View.VISIBLE
+        view.otplayout.visibility=View.VISIBLE
+        firebaseAuth= FirebaseAuth.getInstance()
+
+        progressDialog= ProgressDialog(activity)
+        progressDialog.setTitle("text")
+        progressDialog.setCanceledOnTouchOutside(false)
+        mCallBack=object :PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+            override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+                Log.d(Tag(),"onverficationcompleted: ")
+                signinwithphoneauth(p0)
+            }
+
+            override fun onVerificationFailed(p0: FirebaseException) {
+                progressDialog.dismiss()
+                Log.d(Tag(),"onVerficationfailed ${p0}")
+                Toast.makeText(activity,"${p0.message}",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onCodeSent(p0: String, p1: PhoneAuthProvider.ForceResendingToken) {
+                Log.d(Tag(),"onCodesent:${p0}")
+                mVerificationId=p0
+                forceResendingToken=p1
+                progressDialog.dismiss()
+                phoneEt.visibility=View.GONE
+                verificationcode.visibility=View.VISIBLE
+                Toast.makeText(activity,"Verification Code sent...",Toast.LENGTH_SHORT).show()
+                captioncode.text="Please type the verification code we sent to ${phoneEt.text.toString().trim()}"
+                super.onCodeSent(p0, p1)
+            }
+
+
+        }
+
+        view.continuebtn.setOnClickListener{
+            //input phone number
+            val phone=phoneEt.text.toString().trim()
+            //validate phone number
+            if(TextUtils.isEmpty(phone)){
+                Toast.makeText(this.activity,"Please enter your phone number...",Toast.LENGTH_SHORT).show()
+
+            }
+            else{
+                startphoneverification(phone)
+            }
+        }
+
+        view.otpresentcode.setOnClickListener {
+            val phone=phoneEt.text.toString().trim()
+            //validate phone number
+            if(TextUtils.isEmpty(phone)){
+                Toast.makeText(this.activity,"Please enter your phone number...",Toast.LENGTH_SHORT).show()
+
+            }
+            else{
+                resendphoneOTPcode(phone,forceResendingToken)
+            }
+        }
+
+        view.submit_btn.setOnClickListener {
+            val phone=phoneEt.text.toString().trim()
+            //validate phone number
+            if(TextUtils.isEmpty(phone)){
+                Toast.makeText(this.activity,"Please enter your phone number...",Toast.LENGTH_SHORT).show()
+
+            }
+            else{
+                verifyphonenumber(mVerificationId, code = "")
+            }
+        }
+        return view
+
+        return view
     }
 
 
